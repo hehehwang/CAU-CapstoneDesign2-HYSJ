28835bdd4244ef7783a823530ef593bd2bcf9d4e
heheHwang <hehehwang@gmail.com>
2021-11-23 20:21:15 +0900

    HolisticLandamarks - prototype

--- components/dataclass.py
+++ components/dataclass.py
@@ -1,26 +1,36 @@
 from typing import Union
 
 import numpy as np
+from mediapipe.framework.formats.landmark_pb2 import Landmark
 from numpy.core.fromnumeric import prod
 
+from .CONST import (
+    HAND_LANDMARK_ID_TO_NAME,
+    HAND_LANDMARK_NAME_TO_ID,
+    POSE_LANDMARK_ID_TO_NAME,
+    POSE_LANDMARK_NAME_TO_ID,
+)
 
-class Coord3d:
+
+class Vector3d:
     def __init__(self, *args: Union[int, float, np.ndarray]):
         if len(args) == 1:
             self.__np = np.array(args[0], dtype=np.float64)
+        elif len(args) == 2:
+            self.__np = np.array([args[0], args[1], 0], dtype=np.float64)
         elif len(args) == 3:
             self.__np = np.array(args, dtype=np.float64)
         else:
-            raise ValueError("Invalid number of arguments")
+            raise ValueError(f"Invalid number of arguments: {len(args)}")
         self.__x = self.__np[0]
         self.__y = self.__np[1]
         self.__z = self.__np[2]
 
-    def unitVector(self) -> "Coord3d":
+    def unitVector(self) -> "Vector3d":
         ans = self.asNp / np.linalg.norm(self.asNp)
-        return Coord3d(ans[0], ans[1], ans[2])
+        return Vector3d(ans[0], ans[1], ans[2])
 
-    def angleBtw(self, other: "Coord3d") -> float:
+    def angleBtw(self, other: "Vector3d") -> float:
         unitVectorA = self.unitVector()
         unitVectorB = other.unitVector()
         product = np.dot(unitVectorA.asNp, unitVectorB.asNp)
@@ -31,8 +41,11 @@ class Coord3d:
         angleRad = np.arccos(product)
         return np.rad2deg(angleRad)
 
+    def distBtw(self, other: "Vector3d") -> float:
+        return np.linalg.norm(self.asNp - other.asNp)
+
     def __eq__(self, __o: object) -> bool:
-        if isinstance(__o, Coord3d):
+        if isinstance(__o, Vector3d):
             return np.isclose(self.asNp, __o.asNp).all()
         return False
 
@@ -59,23 +72,64 @@ class Coord3d:
         return self.__str__()
 
 
-class Landmark(Coord3d):
-    def __init__(
-        self,
-        x: Union[int, float],
-        y: Union[int, float],
-        z: Union[int, float],
-        vis: Union[int, float],
-    ):
-        super().__init__(x, y, z)
-        self.__vis = vis
+class MyLandmark(Vector3d):
+    def __init__(self, *args):
+        if len(args) == 4:
+            __x, __y, __z, __vis = args
+            super().__init__(__x, __y, __z)
+        elif len(args) == 3:
+            __x, __y, __vis = args
+            super().__init__(__x, __y, 0)
+        else:
+            raise ValueError(f"Invalid number of arguments: {len(args)}")
+        self.__vis = __vis
 
     @property
     def vis(self) -> Union[int, float]:
         return self.__vis
 
     def __str__(self) -> str:
-        return f"({super().__str__()}): {self.__vis}"
+        return f"{super().__str__()}: {self.__vis}"
 
     def __repr__(self) -> str:
         return self.__str__()
+
+
+class HolisticLandmarks:
+    def __init__(self, MPResults):
+        __poseWorldLandmarks = MPResults.pose_world_landmarks.landmark
+        __lefthandLandmarks = (
+            MPResults.left_hand_landmarks.landmark
+            if MPResults.left_hand_landmarks
+            else []
+        )
+        __righthandLandmarks = (
+            MPResults.right_hand_landmarks.landmark
+            if MPResults.right_hand_landmarks
+            else []
+        )
+        self.__landmarks = {
+            "leftSide": {"pose": {}, "hand": {}},
+            "rightSide": {"pose": {}, "hand": {}},
+        }
+        for i, pwlm in enumerate(__poseWorldLandmarks):
+            landmark = MyLandmark(pwlm.x, pwlm.y, pwlm.z, pwlm.visibility)
+            dest = self.__landmarks["leftSide" if i % 2 else "rightSide"]
+            dest["pose"][str(i)] = landmark
+            dest["pose"][POSE_LANDMARK_ID_TO_NAME[i]] = landmark
+
+        for i, hlm in enumerate(__lefthandLandmarks):
+            landmark = MyLandmark(hlm.x, hlm.y, hlm.visibility)
+            dest = self.__landmarks["leftSide"]["hand"]
+            dest[str(i)] = landmark
+            dest[HAND_LANDMARK_ID_TO_NAME[i]] = landmark
+
+        for i, hlm in enumerate(__righthandLandmarks):
+            landmark = MyLandmark(hlm.x, hlm.y, hlm.visibility)
+            dest = self.__landmarks["rightSide"]["hand"]
+            dest[str(i)] = landmark
+            dest[HAND_LANDMARK_ID_TO_NAME[i]] = landmark
+
+    @property
+    def landmarks(self) -> dict:
+        return self.__landmarks
