afdd9eb475e143b6d9250acc1d0409a4f1456dba
heheHwang <hehehwang@gmail.com>
2021-12-02 15:54:25 +0900

    update installs.sh

--- installs.sh
+++ installs.sh
@@ -1,2 +1,3 @@
-sudo apt-get install build-essentialpython3-pip python3-dev python3-smbus -y
+sudo apt-get install build-essential python3-pip python3-dev python3-smbus -y
 sudo pip3 install adafruit-circuitpython-pca9685 adafruit-circuitpython-motor -y
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
